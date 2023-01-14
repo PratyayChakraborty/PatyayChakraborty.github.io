d8088088c8abe52000c2cf0c89bec76dce1e3f4b
PratyayChakraborty <pratyaychakraborty024@gmail.com>
2023-01-15 00:44:25 +0530

    added

--- app.js
+++ app.js
@@ -24,5 +24,9 @@ document.getElementById("resumehoo1").addEventListener("click",re_fun);
 
 function re_fun() { 
    // window.location="https://drive.google.com/file/d/19Pvl7XdGm6eeHpYNGD02X9J73Ksle-Xq/view?usp=share_link"
-   window.location.href = "https://drive.google.com/file/d/19Pvl7XdGm6eeHpYNGD02X9J73Ksle-Xq/view?usp=share_link";
+   // window.location="_blank";
+  
+   window.open("https://drive.google.com/file/d/1sIRCdL4g82GgpngzUJyXONzbOltI4Jdx/view?usp=share_link");
+   // window.location.href = "https://drive.google.com/file/d/1sIRCdL4g82GgpngzUJyXONzbOltI4Jdx/view?usp=share_link";
+   // window.location.href = "https://drive.google.com/file/d/19Pvl7XdGm6eeHpYNGD02X9J73Ksle-Xq/view?usp=share_link";
 }
